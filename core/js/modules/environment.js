88dcc82b3f988289122001db38e6d0ba5f25ebc9
Stephane Jomphe <SJOMPHE@csps-efpc.com>
2020-03-09 08:22:00 -0400

    environment

--- core/js/modules/environment.js
+++ core/js/modules/environment.js
@@ -1,86 +1,84 @@
 define(['jquery',
-		'settings-core'], function ($,CoreSettings) {
-    'use strict';  
+	'settings-core'
+], function ($, CoreSettings) {
+	'use strict';
 	var api;
-	
+
 	//what's environment (local, skynet, publicfacing)
-		CoreSettings.environment=(CoreSettings.environment===null)?detectHost():CoreSettings.environment;
+	CoreSettings.environment = (CoreSettings.environment === null) ? detectHost() : CoreSettings.environment;
 
 	//what kind of data storage does it have (connectionMode)
-		api=getConnection();
+	api = getConnection();
 
-		CoreSettings.connectionMode=(CoreSettings.connectionMode===null)?detectConnMode(api):CoreSettings.connectionMode;
-	
+	CoreSettings.connectionMode = (CoreSettings.connectionMode === null) ? detectConnMode(api) : CoreSettings.connectionMode;
 
-		CoreSettings.scormversion=api;
-	
-	
+
+	CoreSettings.scormversion = api;
+
 
 	$("html").attr("data-pop", CoreSettings.isPopped)
 	$("html").attr("data-env", CoreSettings.environment)
 	$("html").attr("data-conn", CoreSettings.connectionMode)
-	
-	
-	
-	
+
 
 	//detect if the host is local, public facing or on a prod environment (will include any saba server)
-	function detectHost(){
+	function detectHost() {
 		var host;
-		   host=(location.hostname.indexOf("10.128") ===0)?"localhost":location.hostname;
-		   
-		   switch(host) {
-				case "localhost":
-				case "127.0.0.1":
-			   	case "skynet":				   
-					host="local";
-					break;
-				case "www.csps-efpc.gc.ca":
-				case "csps-efpc.gc.ca":
-					host= "public";
-					break;
-				default:
-					host= "prod";
-					break;
-			}
+		host = (location.hostname.indexOf("10.128") === 0) ? "localhost" : location.hostname;
+
+		switch (host) {
+			case "localhost":
+			case "127.0.0.1":
+			case "skynet":
+			case "skynet-1":
+				host = "local";
+				break;
+			case "www.csps-efpc.gc.ca":
+			case "csps-efpc.gc.ca":
+				host = "public";
+				break;
+			default:
+				host = "prod";
+				break;
+		}
+		console.log(host);
 		return host;
 
 	}
-	
-	function detectConnMode(api){
+
+	function detectConnMode(api) {
 
 		//CoreSettings.connectionMode=(CoreSettings.connectionMode===null)?null:CoreSettings.connectionMode;
-		if(CoreSettings.environment!=="prod" || api === null){
+		if (CoreSettings.environment !== "prod" || api === null) {
 			return "offline";
-		}else{
+		} else {
 			return "scorm";
 		}
-		
-		
-		
-		
+
+
 	}
-	function getConnection(){
-		if(CoreSettings.isPopped === true){
-			if(window.opener.getAPIHandle() !== null){
-				CoreSettings.api=window.opener;
+
+	function getConnection() {
+		if (CoreSettings.isPopped === true) {
+			if (window.opener.getAPIHandle() !== null) {
+				CoreSettings.api = window.opener;
 				return window.opener.getAPIHandle();
 			}
-		}else{
-			if(getAPIHandle() !== null){
-				CoreSettings.api=window;
+		} else {
+			if (getAPIHandle() !== null) {
+				CoreSettings.api = window;
 				return getAPIHandle();
-			}else{
+			} else {
 				return null;
 			}
-		}				
+		}
 	}
-		function scormCheck(){
 
-			//scormVersion
-			return getAPIHandle().scormVersion !== null;
-		}	
-	
-	
+	function scormCheck() {
+
+		//scormVersion
+		return getAPIHandle().scormVersion !== null;
+	}
+
+
 });
-
